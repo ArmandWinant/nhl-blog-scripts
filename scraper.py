914e883ff629acd7b518f87fe345554b9b088e7c
Bastien Winant <babw1@student.london.ac.uk>
2022-05-31 12:18:13 +0200

    move Scraper class definition to a separate file

---
+++ scraper.py
@@ -0,0 +1,56 @@
+from extract import open_driver, close_driver, wait_for_element
+from DML import db_connect, close_db_connect
+from selenium.webdriver.common.by import By
+from psycopg2.extras import execute_batch
+
+
+def get_table_columns(table_element):
+    header_container = wait_for_element(source=table_element, search_by=By.CLASS_NAME, target="table-header-container", unique_element=True)
+    header_elements = wait_for_element(source=header_container, search_by=By.CLASS_NAME, target="rt-th")
+    header_names = [header.text for header in header_elements]
+    
+    return header_names
+
+
+def get_table_rows(table_element):
+    table_rows = wait_for_element(source=table_element, search_by=By.CLASS_NAME, target="rt-tbody", unique_element=True)
+    rows = wait_for_element(source=table_rows, search_by=By.CLASS_NAME, target="rt-tr")
+    
+    return rows
+
+
+class Scraper:
+    def __init__(self):
+        self.driver = None
+        self.conn = None
+        self.cur = None
+        self.table_data = None
+        self.table_headers = None
+        self.table_insert = ""
+        self.staged_data = []
+        
+        self.url_dict = {
+            "aggregate": 0,
+            "reportType": "game",
+            "pageSize": 100
+        }
+    
+    def open_driver(self):
+        self.driver = open_driver()
+    
+    def close_driver(self):
+        if self.driver:
+            close_driver(self.driver)
+            
+    def db_connect(self):
+        self.conn, self.cur = db_connect()
+    
+    def close_db_connect(self):
+        if self.conn and self.cur:
+            close_db_connect(self.conn, self.cur)
+
+    def load(self):
+        execute_batch(self.cur, self.table_insert, self.staged_data)
+        print(f"Loaded {len(self.staged_data)} records")
+        
+        
