9746062100f19a92372a7179260d100663ddf654
Bastien Winant <babw1@student.london.ac.uk>
2022-06-11 11:02:09 +0200

    Create goalsByPeriodScraper.py

---
+++ goalsByPeriodScraper.py
@@ -0,0 +1,101 @@
+from extract import open_driver, close_driver, wait_for_element
+from transforms import parse_record, parse_game_date, game_season
+from selenium.webdriver.common.by import By
+from scraper import Scraper, get_table_columns, get_table_rows
+from sql_queries import shot_attempts_table_insert
+import urllib.parse
+from datetime import datetime
+import time
+
+
+class GoalsByPeriodScraper(Scraper):
+    def __init__(self):
+        super().__init__()
+        self.table_insert = shot_attempts_table_insert
+        self.url_dict["report"]= "goalsbyperiod"
+    
+    
+    def transform(self, headers, row_elements):
+        playoffs = self.url_dict["gameType"] == 3
+        
+        for row in row_elements:
+            row_cells = wait_for_element(source=row, search_by=By.CLASS_NAME, target="rt-td")
+            row_values = [cell.text for cell in row_cells]
+            
+            values_map = list(zip(headers, row_values))
+            map_dict = parse_record(values_map)
+
+            self.cur.execute("SELECT abbreviation FROM teams WHERE team = %s;", (map_dict["Team"],))
+            team_abbreviation = self.cur.fetchone()[0]
+            
+            ordered_data_list = [
+                team_abbreviation,
+                map_dict["game_date"],
+                map_dict["EV GF"],
+                map_dict["PP GF"],
+                map_dict["SH GF"],
+                map_dict["GF in P1"],
+                map_dict["GF in P2"],
+                map_dict["GF in P3"],
+                map_dict["GF in OT"],
+                map_dict["GA in P1"],
+                map_dict["GA in P2"],
+                map_dict["GA in P3"],
+                map_dict["GA in OT"]
+            ]
+                    
+            self.staged_data.append(ordered_data_list)
+            
+            
+    def extract(self, url):
+        """sends requests to the url and scrapes the table elements (header and rows)"""
+        self.url_dict["page"] = 0
+        self.staged_data = []
+        errors = 0
+        
+        while True:
+            url = self.base_url + urllib.parse.urlencode(self.url_dict)
+
+            self.driver.get(url)
+            try:
+                # scrape the page's main table
+                root_element = wait_for_element(source=self.driver, search_by=By.ID, target="root", unique_element=True)
+
+                pagination = wait_for_element(source=root_element, search_by=By.CLASS_NAME, target="pagination", unique_element=True)
+                page_number_field = wait_for_element(source=pagination, search_by=By.TAG_NAME, target="input", unique_element=True)
+                total_pages = int(page_number_field.get_attribute('max'))
+
+                data_table = wait_for_element(source=root_element, search_by=By.CLASS_NAME, target="rt-table", unique_element=True)
+
+                table_headers = get_table_columns(data_table)
+                row_elements = get_table_rows(data_table)
+
+                self.transform(table_headers, row_elements)
+
+                self.url_dict["page"] += 1
+
+                errors = 0
+
+            except AttributeError:
+                errors += 1
+
+                if errors == 3:
+                    self.url_dict["page"] += 1
+                    print("Skipped page ", self.url_dict["page"])
+            
+            if self.url_dict["page"] >= total_pages:
+                break
+                    
+                    
+    def etl(self, start, playoffs=False):
+        # the chrome driver and database cursor are used in multiple scripts
+        self.open_driver()
+        self.db_connect()
+        
+        self.build_url(start=start, playoffs=playoffs)
+        self.extract(start)
+        self.load()
+        
+        # close the driver and the cursor
+        self.close_driver()
+        self.close_db_connect()
